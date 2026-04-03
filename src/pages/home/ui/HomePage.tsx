03544649db8852ceb80ae3dbbd31464d6c149699
Aiypkhan Madiyar <madiyar.talgatuly@inbox.ru>
2026-04-03 17:18:25 +0500

    feat: added components

---
+++ src/pages/home/ui/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="container">
+        .
+        <h1 className="text-4xl font-bold">Home Page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
